8cf3906d63c6c315209d8e9d32f61ab64b8ad97d
emnghiaproplayers <truongnghia297@gmail.com>
2026-02-07 22:42:17 +0700

    Find Kth Node From The End

---
+++ Linked List/find-kth-node-from-the-end.js
@@ -0,0 +1,111 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+ 
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+    }
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+ 
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+    
+   findKthFromEnd(k) {
+    let slow = this.head;
+    let fast = this.head;
+
+    // Bước 1: cho fast đi trước k bước
+    for (let i = 0; i < k; i++) {
+        if (fast === null) return null; // k lớn hơn length
+        fast = fast.next;
+    }
+
+    // Bước 2: slow và fast cùng đi
+    while (fast !== null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    // Bước 3: slow chính là kth node từ cuối
+    return slow;
+}
+
+}
+
+
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+
+console.log("Original list:");
+myLinkedList.printList();
+
+const k = 2;
+const kthNodeFromEnd = myLinkedList.findKthFromEnd(k);
+
+console.log(`\n${k}th node from the end:`);
+if (kthNodeFromEnd) {
+    console.log(kthNodeFromEnd.value);
+} else {
+    console.log("Not found");
+}
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    4
+    5
+    2th node from the end:
+    4
+*/
